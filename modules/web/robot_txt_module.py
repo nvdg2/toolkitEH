6a402bf9e22f4bb104a854da1f3af57956cf9add
Niels Van De Ginste <niels.vandeginste@student.ap.be>
2023-12-08 19:40:03 +0100

    chore: add argparse support to robot_txt_module.py

--- modules/web/robot_txt_module.py
+++ modules/web/robot_txt_module.py
@@ -5,10 +5,9 @@ import json
 import pathlib
 import os
 
-def perform_wordlist_attack(target, list):
-    print(list)
+def perform_wordlist_attack(target, list_path):
     try:
-        wordlist=read_list(f"resources/wordlists/{list}.txt")
+        wordlist=read_list(list_path)
     except:
         return "worldlist not found"
     try:
@@ -53,22 +52,29 @@ def read_list(list_path):
             wordlist.append(line.replace("\n",""))
     return wordlist
 
-def log_results(urls):
+def log_results(result):
     timestamp = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     xss_dir = pathlib.Path("logs/wordlist_attack")
     xss_dir.mkdir(exist_ok=True,parents=True)
     save_location=xss_dir/f"wordlist_attack_{timestamp}.json"
     os.chown(xss_dir,1000,1000)
     with save_location.open("w") as json_file_raw:
-        json.dump(urls, json_file_raw)
+        json.dump(result, json_file_raw)
 
 def main():
-    parser = argparse.ArgumentParser(description="Website wordlist attack")
-    parser.add_argument("target", help="target url for wordlist attack")
-    parser.add_argument("list", help="use the standard wordlist",choices=["standard", "swagger"])
+    parser = argparse.ArgumentParser(description="Brute force wordlist attack uitvoeren op een website")
+    parser.add_argument("target", help="doelurl voor wordlist attack")
+    parser.add_argument("-l", "--standard_list_choice", help="Standaard keuzelijst voor bruteforce aanval. Indien geen -c flag gebruikt",choices=["standard", "swagger"])
+    parser.add_argument("-c", "--custom", action="store_true", default=False, help="Gebruik deze flag om aan te geven dat een custom lijst opgegeven wordt")
+    parser.add_argument("-p", "--list_path", help="Locatie van custom lijst")
+    
     args = parser.parse_args()
     
-    perform_wordlist_attack(target=args.target, list=args.list)
+    if args.custom:
+        perform_wordlist_attack(target=args.target, list_path=args.list_path)
+    else:
+        perform_wordlist_attack(target=args.target, list_path=f"resources/wordlists/{args.standard_list_choice}.txt")
+        
 
 if __name__=="__main__":
     main()
